b8d9733c1d537780c7508296d3518b179d1081be
yukishi24 <yukishi24github@gmail.com>
2023-04-26 23:27:24 +0900

    add: HelloController.java
    =content=
    ・point1: @Controllerアノテーション画面などからHTTPリクエストを受け付けるクラスのことをコントローラーと呼ぶ。@Controllerアノテーションを付与するとコントローラーとなる。
    ・point2: @GetMappingアノテーションGetメソッドのHTTPリクエストを受け付けるために使用する。後ろに引数としてURLを記述する。

---
+++ app/src/main/java/com/example/demo/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String getHello() {
+		//hello.htmlに遷移する
+		return "hello";
+	}
+}
