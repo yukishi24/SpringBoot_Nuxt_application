4e07aacbcfe1d45292bb4ae5491c981bd571d1a6
yukishi24 <yukishi24github@gmail.com>
2023-04-28 22:23:31 +0900

    modify: HelloController.java
    =CONTEINT=
    ・PostMappingアノテーション:HTTPリクエストのpostを受け取ることが可能。
    ・RequestParamアノテーション:HTMLのname属性と一致させることでパラメーターを取得することが可能。

--- app/src/main/java/com/example/demo/hello/HelloController.java
+++ app/src/main/java/com/example/demo/hello/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.demo.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,5 +13,14 @@ public class HelloController {
 	public String getHello() {
 		//hello.htmlに遷移する
 		return "hello";
+	}
+
+	@PostMapping("/hello")
+	public String postRequest(@RequestParam("text1") String str, Model model) {
+		//受け取ったパラメータをmodelに登録する
+		model.addAttribute("sample", str);
+
+		//response.htmlに遷移
+		return "/hello/response";
 	}
 }
