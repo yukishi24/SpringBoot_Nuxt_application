be3d4fcfff5ed39cdcd72368644d8d1ca1d89c33
yukishi24 <yukishi24github@gmail.com>
2023-04-30 21:20:54 +0900

    add: LoginController.java

---
+++ app/src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	//ログイン画面に遷移
+	@GetMapping("/login")
+	public String getLogin() {
+		return "/login/login";
+	}
+}
