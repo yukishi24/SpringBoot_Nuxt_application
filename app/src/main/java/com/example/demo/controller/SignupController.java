916880d776e814e546ec6e80a6ba24ce28243d7c
yukishi24 <yukishi24github@gmail.com>
2023-05-02 23:44:29 +0900

    modify: SignupController.java
    -content-
    ・BindingResult -> バインド、バリテーションエラーが発生しているのかどうかは、BindingResultのメソッドで確認することが可能。hasErrors()メソッドの結果がtrueとなる場合、バインドエラーかバリテーションエラーが発生する。

--- app/src/main/java/com/example/demo/controller/SignupController.java
+++ app/src/main/java/com/example/demo/controller/SignupController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +27,7 @@ public class SignupController {
 
 	//ユーザー登録画面に遷移
 	@GetMapping("/signup")
-	public String getSignup(Model model,Locale locale, @ModelAttribute SignupForm form) {
+	public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form) {
 		//性別を取得
 		Map<String, Integer> genderMap = applicationService.getGenderMap(locale);
 		model.addAttribute("genderMap", genderMap);
@@ -37,10 +38,16 @@ public class SignupController {
 
 	//ユーザー登録処理
 	@PostMapping("/signup")
-	public String postSignup(@ModelAttribute SignupForm form) {
-		
+	public String postSignup(Model model, Locale locale, @ModelAttribute SignupForm form, BindingResult bindingResult) {
+
+		//入力チェック
+		if (bindingResult.hasErrors()) {
+			//NG:ユーザー登録画面に遷移
+			return getSignup(model, locale, form);
+		}
+
 		log.info(form.toString());
-		
+
 		//ログイン画面にリダイレクト
 		return "redirect:/login";
 	}
