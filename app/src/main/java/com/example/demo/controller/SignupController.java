7f338f3e1b4e6c390ad07308b72d52f50ae0eacb
yukishi24 <yukishi24github@gmail.com>
2023-05-05 21:46:04 +0900

    modify: SignupController.java
    -content-
    ・@Validatedを使用するとバリデーションが実行される。

--- app/src/main/java/com/example/demo/controller/SignupController.java
+++ app/src/main/java/com/example/demo/controller/SignupController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,7 +39,7 @@ public class SignupController {
 
 	//ユーザー登録処理
 	@PostMapping("/signup")
-	public String postSignup(Model model, Locale locale, @ModelAttribute SignupForm form, BindingResult bindingResult) {
+	public String postSignup(Model model, Locale locale, @ModelAttribute @Validated SignupForm form, BindingResult bindingResult) {
 
 		//入力チェック
 		if (bindingResult.hasErrors()) {
