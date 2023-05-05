75a2db046b5041e7b05581806386bc4fe59986cb
yukishi24 <yukishi24github@gmail.com>
2023-05-05 21:46:29 +0900

    modify: SignupForm.java

--- app/src/main/java/com/example/demo/form/SignupForm.java
+++ app/src/main/java/com/example/demo/form/SignupForm.java
@@ -5,7 +5,15 @@ package com.example.demo.form;
 
 import java.util.Date;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 /**
  * 画面の入力内容とマッピングするフォームクラス
@@ -14,12 +22,27 @@ import org.springframework.format.annotation.DateTimeFormat;
  *
  */
 public record SignupForm(
-    String userId,
+	@NotBlank
+	@Email
+    String userId, 
+    
+    @NotBlank
+    @Length(max = 100, min = 4)
+	@Pattern(regexp = "^[a-zA-Z0-9]+$")
     String password,
+    
+    @NotBlank
     String userName,
+    
     @DateTimeFormat(pattern = "yyyy/MM/dd")
+	@NotNull
     Date birthday,
+    
+    @Min(20)
+	@Max(100)
     Integer age,
+    
+    @NotNull
     Integer gender
     ) {
 
