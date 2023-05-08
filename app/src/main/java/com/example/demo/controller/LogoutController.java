3da9fe396ed579384b49ce23349a0baceffce8f8
yukishi24 <yukishi24github@gmail.com>
2023-05-08 23:11:59 +0900

    modify: LogoutController.java

--- app/src/main/java/com/example/demo/controller/LogoutController.java
+++ app/src/main/java/com/example/demo/controller/LogoutController.java
@@ -5,11 +5,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * ログアウト画面に遷移する時の処理を統括するコントローラー
+ * 
+ * @author yukishi
+ *
+ */
 @Controller
 @Slf4j
 public class LogoutController {
 
-	//ログイン画面にリダイレクト
+	/**
+	 * ログイン画面にリダイレクト
+	 * 
+	 * @return　login.ttml
+	 */
 	@PostMapping("/logout")
 	public String postLogout() {
 		log.info("ログアウト");
