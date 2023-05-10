231d856f92643a8a6d2565f282153d51f473e77b
yukishi24 <yukishi24github@gmail.com>
2023-05-10 22:58:39 +0900

    add:

--- app/src/main/java/com/example/demo/controller/SignupController.java
+++ app/src/main/java/com/example/demo/controller/SignupController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.Locale;
 import java.util.Map;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.application.service.UserApplicationService;
+import com.example.demo.domain.user.model.MUser;
+import com.example.demo.domain.user.service.UserService;
 import com.example.demo.form.GroupOrder;
 import com.example.demo.form.SignupForm;
 
@@ -32,6 +35,8 @@ public class SignupController {
 
 	@Autowired
 	private UserApplicationService applicationService;
+	private UserService userService;
+	private ModelMapper modelMapper;
 	
 	/**
 	 * ユーザー登録画面に遷移<br>
@@ -75,6 +80,12 @@ public class SignupController {
 		}
 
 		log.info(form.toString());
+		
+		//formをMUserクラスに変換
+		MUser user = modelMapper.map(form, MUser.class);
+		
+		//ユーザ登録
+		userService.signup(user);
 
 		//ログイン画面にリダイレクト
 		return "redirect:/login";
